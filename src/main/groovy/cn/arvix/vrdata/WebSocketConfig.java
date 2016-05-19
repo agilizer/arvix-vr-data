271f44fb786ff42dcdad9dbef19f886e024ceea1
asdtiang <asdtiangxia@163.com>
2016-05-19 16:04:23 +0800

    fix bugs

--- src/main/groovy/cn/arvix/vrdata/WebSocketConfig.java
+++ src/main/groovy/cn/arvix/vrdata/WebSocketConfig.java
@@ -25,7 +25,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(myHandler(), "/websocket/endpoint")
             .setAllowedOrigins("*")
-            .withSockJS();
+            .withSockJS().setHttpMessageCacheSize(2000);
     }
 
     @Bean
